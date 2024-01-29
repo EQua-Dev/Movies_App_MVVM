4ea38f67ce359a74a4ccd9e4fdb59e9b625983e4
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-01-29 05:58:53 +0100

    set up project structure

---
+++ MoviesApp/Models/Movie.swift
@@ -0,0 +1,32 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by Richard Uzor on 29/01/2024.
+//  Copyright © 2024 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct MovieResponse: Codable{
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey{
+        case movies = "Search"
+    }
+}
+
+
+struct Movie: Codable{
+    let title: String
+    let year: String
+    let imdbId: String
+    let poster: String
+    
+    private enum CodingKeys: String, CodingKey{
+        case title = "Title"
+        case year = "Year"
+        case imdbId = "imdbID"
+        case poster = "Poster"
+    }
+}
