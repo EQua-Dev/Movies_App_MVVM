c52874a5fac1c7535aff25c16d23953768d355ae
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-01-30 21:05:26 +0100

    implemented movieDetail model

---
+++ MoviesApp/Models/MovieDetail.swift
@@ -0,0 +1,34 @@
+//
+//  MovieDetail.swift
+//  MoviesApp
+//
+//  Created by Richard Uzor on 30/01/2024.
+//  Copyright © 2024 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+
+struct MovieDetail: Decodable{
+    let title: String;
+    let year: String;
+    let rated: String;
+    let plot: String;
+    let director: String;
+    let actors: String;
+    let imdbRating: String;
+    let poster: String;
+    let imdbId: String;
+    
+    private enum CodingKeys: String, CodingKey{
+        case title = "Title"
+        case year = "Year"
+        case rated = "Rated"
+        case plot = "Plot"
+        case director = "Director"
+        case actors = "Actors"
+        case imdbId = "imdbID"
+        case imdbRating = "imdbRating"
+        case poster = "Poster"
+        
+    }
+}
